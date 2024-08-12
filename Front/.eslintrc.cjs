843ea3b7edd511c6a6a2462147b9c307a58f0cea
Shay_Batonya <127010513+ShaiBatonya@users.noreply.github.com>
2024-08-12 12:26:21 +0300

    v2

--- Front/.eslintrc.cjs
+++ Front/.eslintrc.cjs
@@ -2,13 +2,15 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh',"@typescript-eslint", "react"],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
